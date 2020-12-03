bdd8589395c44d613ce2f832621515b3325b6289
knowther <johnnywesley.contato@outlook.com>
2020-12-03 00:58:10 -0300

    Agora ao clickar no botão chat da nav vai mudar pra tela de chat que ainda está em desenvolvimento

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function trocarchat(){
+    document.getElementById("main").style.display = "none";
+    document.getElementById("chats").style.display = "flex";
+    document.getElementById("chat-icon").style.color = "#1be4a1";
+    document.getElementById("tinder-icon").style.color = "gray";
+}
