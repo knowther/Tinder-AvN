aadb6eb04c1fff5f1f2d156f1428f348ac97f4c1
knowther <johnnywesley.contato@outlook.com>
2020-12-03 01:04:01 -0300

    agora do menu chat volta pro principal

--- js/script.js
+++ js/script.js
@@ -2,5 +2,11 @@ function trocarchat(){
     document.getElementById("main").style.display = "none";
     document.getElementById("chats").style.display = "flex";
     document.getElementById("chat-icon").style.color = "#1be4a1";
-    document.getElementById("tinder-icon").style.color = "gray";
+    document.getElementById("tinder-icon").style.color = "#dadfe6";
+}
+function chat_tinder(){
+    document.getElementById("chats").style.display = "none";
+    document.getElementById("main").style.display = "flex";
+    document.getElementById("chat-icon").style.color = "#dadfe6";
+    document.getElementById("tinder-icon").style.color = "#fe466d";
 }
